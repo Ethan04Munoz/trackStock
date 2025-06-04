e5dc70ff52106067501027625443bc22ab5c9c8e
Ethan <ethanmunozlopez04@gmail.com>
2025-06-03 18:43:33 -0600

    Replaced mockup sidebar for actual sidebar

--- client/src/Pages/Main.jsx
+++ client/src/Pages/Main.jsx
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from 'react';
 import Dashboard from '../components/Dashboard';
 import './Main.css';
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 
 function Main() {
     return (
         <div className="main">
-            <Header />
+            <Header page="/"/>
             <div className="page">
-                <div className="sidebar">
+                {/*<div className="sidebar">
                     mockup
-                </div>
+                </div>*/}
+                <Sidebar />
 
                 <Dashboard />
             </div>
