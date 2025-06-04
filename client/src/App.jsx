ac4227e676a7a5c2e1f64da526bd8dc0fccc01c6
Ethan <ethanmunozlopez04@gmail.com>
2025-06-03 18:43:02 -0600

    Created Test page (To explain to the user how to test the app)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import StockSearch from './components/Search';
 import Main from './Pages/Main';
+import About from './Pages/About';
+import Test from './Pages/Test';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />}>
-        </Route>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/test" element={<Test />}></Route>
         {/*<Route path="/test" element={<StockSearch />}></Route>*/}
       </Routes>
     </BrowserRouter>
---
+++ client/src/Pages/Test.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Test() {
+    return (
+        <div className="about">
+            <Header page="/test" />
+            <h1>Test TrackStock</h1>
+            <p>
+                You can test the TrackStock application here.
+                This is a web application for educational purposes, designed to help users track
+                their stock investments and take a desicion about if a stock is a good buy at the
+                moment, and you can test all of the features here. However, please note that
+                the AI features are powered by the Gemini API, which requires a key. This app is designed
+                to manage securely the key, however, my best recommendation is that, if you want to use
+                the AI features, you should download the app from GitHub and run it locally. 
+            </p>
+            <h2>Follow the next steps to get this working locally in your computer</h2>
+
+        </div>
+    );
+}
+
+export default Test;
