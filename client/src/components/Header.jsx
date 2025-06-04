0bc55b87616ffdec0dca4b80174da2d5669fd308
Ethan <ethanmunozlopez04@gmail.com>
2025-06-03 18:42:08 -0600

    Created a Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './Header.css';
 
-function Header() { 
-    return(
+function Header(props) {
+    const page = props.page;
+    return (
         <div className="header">
-            mockup
+            <div className="mainHeader">
+                <h1>TrackStock</h1>
+            </div>
+            <div className="barHeader">
+                <div className="main">
+                    <Link
+                        className={`link ${page === "/" ? "currentPage" : ""}`}
+                        to="/"
+                    >
+                        Main
+                    </Link>
+                </div>
+                <div className="about">
+                    <Link
+                        className={`link ${page === "/about" ? "currentPage" : ""}`}
+                        to="/about"
+                    >
+                        About
+                    </Link>
+                </div>
+                <div className="test">
+                    <Link
+                        className={`link ${page === "/test" ? "currentPage" : ""}`}
+                        to="/test"
+                    >
+                        Test
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
